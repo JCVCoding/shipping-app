cb9a2e5a62a6dc7ccc9c352cabe9eca2b53ff7b5
josh <jvcoding9407@gmail.com>
2024-03-20 21:57:23 -0400

    Add form validation to the shipping form

--- src/components/ShippingForm.tsx
+++ src/components/ShippingForm.tsx
@@ -1,122 +1,140 @@
-import { Button, MenuItem, Stack, TextField } from '@mui/material';
-import Grid from '@mui/material/Unstable_Grid2';
-import { FormEvent } from 'react';
-import { Link } from 'react-router-dom';
-import { usaStates } from 'typed-usa-states';
-import { useAppDispatch, useAppSelector } from '../hooks';
-import { updateFromZIP } from '../features/fromZIP/fromZIPSlice';
-import { updateToZIP } from '../features/toZIP/toZIPSlice';
-import { updateShipForm } from '../features/ship/shipSlice';
+import { Button, MenuItem, Stack, TextField } from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2";
+import { useNavigate } from "react-router-dom";
+import { usaStates } from "typed-usa-states";
+import { useAppDispatch, useAppSelector } from "../hooks";
+import { updateFromZIP } from "../features/fromZIP/fromZIPSlice";
+import { updateToZIP } from "../features/toZIP/toZIPSlice";
+import { updateShipForm } from "../features/ship/shipSlice";
+
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type ShippingFormFields = {
+  shipperName: string;
+  shipperAddress: string;
+  shipperCity: string;
+  shipperState: string;
+  shipperZip: string;
+  shipperEmail: string;
+  recipientName: string;
+  recipientCountry: string;
+  recipientAddress: string;
+  recipientCity: string;
+  recipientState: string;
+  recipientZip: string;
+  recipientEmail: string;
+};
 
 const ShippingForm = () => {
   const ship = useAppSelector((state) => state.ship);
   const toZIP = useAppSelector((state) => state.toZIP.value);
   const fromZIP = useAppSelector((state) => state.fromZIP.value);
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ShippingFormFields>();
+
+  const onSubmit: SubmitHandler<ShippingFormFields> = async (data) => {
+    console.log(data);
+    navigate("/ship-details");
   };
+
+  const dispatchToStore = (
+    target: EventTarget & (HTMLTextAreaElement | HTMLInputElement)
+  ) => {
+    dispatch(updateShipForm({ value: target.value, name: target.name }));
+  };
+
   return (
-    <form onSubmit={(e) => handleSubmit(e)}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <Grid container columnSpacing={8} rowSpacing={8}>
         <Grid xs={12} sm={6}>
           <Stack spacing={2}>
             <TextField
-              label='Your Name'
-              type='text'
-              variant='standard'
-              id='your_name'
-              name='shipperName'
+              label="Your Name"
+              type="text"
+              variant="standard"
+              id="your_name"
+              {...register("shipperName", { required: "Name required" })}
+              error={errors.shipperName?.type ? true : false}
+              helperText={errors.shipperName?.message}
               value={ship.shipperName}
-              onChange={({ target }) =>
-                dispatch(
-                  updateShipForm({ value: target.value, name: target.name })
-                )
-              }
+              onInput={({ target }) => dispatchToStore(target)}
               fullWidth
             />
             <TextField
               select
               fullWidth
-              label='Country'
-              id='country_from'
+              label="Country"
+              id="country_from"
               defaultValue={ship.shipperCountry}
-              name='shipperCountry'
-              onChange={({ target }) =>
-                dispatch(
-                  updateShipForm({ value: target.value, name: target.name })
-                )
-              }
-              variant='standard'
+              name="shipperCountry"
+              onInput={({ target }) => dispatchToStore(target)}
+              variant="standard"
             >
-              <MenuItem value={'US'}>United States of America</MenuItem>
+              <MenuItem value={"US"}>United States of America</MenuItem>
             </TextField>
             <TextField
-              label='Street Address'
-              id='address_from'
-              type='text'
-              variant='standard'
+              label="Street Address"
+              id="address_from"
+              type="text"
+              variant="standard"
               value={ship.shipperAddress}
-              name='shipperAddress'
-              onChange={({ target }) =>
-                dispatch(
-                  updateShipForm({ value: target.value, name: target.name })
-                )
-              }
+              {...register("shipperAddress", { required: "Address required" })}
+              error={errors.shipperAddress?.type ? true : false}
+              helperText={errors.shipperAddress?.message}
+              onInput={({ target }) => dispatchToStore(target)}
               fullWidth
             />
             <TextField
-              label='Apt, Floor, Suite, etc.'
-              id='address_2_from'
-              type='text'
-              variant='standard'
+              label="Apt, Floor, Suite, etc."
+              id="address_2_from"
+              type="text"
+              variant="standard"
               value={ship.shipperAddress2}
-              name='shipperAddress2'
-              onChange={({ target }) =>
-                dispatch(
-                  updateShipForm({ value: target.value, name: target.name })
-                )
-              }
+              name="shipperAddress2"
+              onInput={({ target }) => dispatchToStore(target)}
               fullWidth
             />
             <TextField
-              label='ZIP'
-              id='ZIP_from'
-              type='text'
-              variant='standard'
+              label="ZIP"
+              id="ZIP_from"
+              type="text"
+              variant="standard"
               value={fromZIP}
-              name='fromZIP'
-              onChange={({ target }) => dispatch(updateFromZIP(target.value))}
+              {...register("shipperZip", { required: "Zip Code required" })}
+              error={errors.shipperZip?.type ? true : false}
+              helperText={errors.shipperZip?.message}
+              onInput={({ target }) => dispatch(updateFromZIP(target.value))}
               fullWidth
             />
             <TextField
-              label='City'
-              id='city_from'
-              type='text'
-              variant='standard'
+              label="City"
+              id="city_from"
+              type="text"
+              variant="standard"
               value={ship.shipperCity}
-              name='shipperCity'
-              onChange={({ target }) =>
-                dispatch(
-                  updateShipForm({ value: target.value, name: target.name })
-                )
-              }
+              {...register("shipperCity", { required: "City required" })}
+              error={errors.shipperCity?.type ? true : false}
+              helperText={errors.shipperCity?.message}
+              onInput={({ target }) => dispatchToStore(target)}
               fullWidth
             />
             <TextField
               select
               fullWidth
-              label='State'
-              id='state_from'
-              variant='standard'
+              label="State"
+              id="state_from"
+              variant="standard"
               value={ship.shipperState}
-              name='shipperState'
-              onChange={({ target }) =>
-                dispatch(
-                  updateShipForm({ value: target.value, name: target.name })
-                )
-              }
+              {...register("shipperState", { required: "State required" })}
+              error={errors.shipperState?.type ? true : false}
+              helperText={errors.shipperState?.message}
+              onInput={({ target }) => dispatchToStore(target)}
             >
               {usaStates.map(({ abbreviation, name }) => (
                 <MenuItem key={abbreviation} value={abbreviation}>
@@ -125,31 +143,25 @@ const ShippingForm = () => {
               ))}
             </TextField>
             <TextField
-              label='Phone'
-              id='phone_from'
-              type='tel'
-              variant='standard'
+              label="Phone (Optional)"
+              id="phone_from"
+              type="tel"
+              variant="standard"
               value={ship.shipperPhone}
-              name='shipperPhone'
-              onChange={({ target }) =>
-                dispatch(
-                  updateShipForm({ value: target.value, name: target.name })
-                )
-              }
+              name="shipperPhone"
+              onInput={({ target }) => dispatchToStore(target)}
               fullWidth
             />
             <TextField
-              label='Email'
-              id='email_from'
-              type='email'
-              variant='standard'
+              label="Email"
+              id="email_from"
+              type="email"
+              variant="standard"
               value={ship.shipperEmail}
-              name='shipperEmail'
-              onChange={({ target }) =>
-                dispatch(
-                  updateShipForm({ value: target.value, name: target.name })
-                )
-              }
+              {...register("shipperEmail", { required: "Email required" })}
+              error={errors.shipperEmail?.type ? true : false}
+              helperText={errors.shipperEmail?.message}
+              onInput={({ target }) => dispatchToStore(target)}
               fullWidth
             />
           </Stack>
@@ -157,133 +169,115 @@ const ShippingForm = () => {
         <Grid xs={12} sm={6}>
           <Stack spacing={2}>
             <TextField
-              label='Recipient Name'
-              id='recipient_name'
-              type='text'
-              variant='standard'
+              label="Recipient Name"
+              id="recipient_name"
+              type="text"
+              variant="standard"
               value={ship.recipientName}
-              name='recipientName'
-              onChange={({ target }) =>
-                dispatch(
-                  updateShipForm({ value: target.value, name: target.name })
-                )
-              }
+              {...register("recipientName", { required: "Name required" })}
+              error={errors.recipientName?.type ? true : false}
+              helperText={errors.recipientName?.message}
+              onInput={({ target }) => dispatchToStore(target)}
               fullWidth
             />
             <TextField
               select
               fullWidth
-              label='Country'
-              id='country_to'
+              label="Country"
+              id="country_to"
               defaultValue={ship.recipientCountry}
-              name='recipientCountry'
-              onChange={({ target }) =>
-                dispatch(
-                  updateShipForm({ value: target.value, name: target.name })
-                )
-              }
-              variant='standard'
+              {...register("recipientCountry", { required: "Name required" })}
+              error={errors.recipientCountry?.type ? true : false}
+              helperText={errors.recipientCountry?.message}
+              onChange={({ target }) => dispatchToStore(target)}
+              variant="standard"
             >
-              <MenuItem value={'US'}>United States of America</MenuItem>
+              <MenuItem value={"US"}>United States of America</MenuItem>
             </TextField>
             <TextField
-              label='Street Address'
-              id='address_to'
-              type='text'
-              variant='standard'
+              label="Street Address"
+              id="address_to"
+              type="text"
+              variant="standard"
               value={ship.recipientAddress}
-              name='recipientAddress'
-              onChange={({ target }) =>
-                dispatch(
-                  updateShipForm({ value: target.value, name: target.name })
-                )
-              }
+              {...register("recipientAddress", { required: "Name required" })}
+              error={errors.recipientAddress?.type ? true : false}
+              helperText={errors.recipientAddress?.message}
+              onInput={({ target }) => dispatchToStore(target)}
               fullWidth
             />
             <TextField
-              label='Apt, Floor, Suite, etc.'
-              id='address_2_to'
-              type='text'
-              variant='standard'
+              label="Apt, Floor, Suite, etc."
+              id="address_2_to"
+              type="text"
+              variant="standard"
               value={ship.recipientAddress2}
-              name='recipientAddress2'
-              onChange={({ target }) =>
-                dispatch(
-                  updateShipForm({ value: target.value, name: target.name })
-                )
-              }
+              name="recipientAddress2"
+              onInput={({ target }) => dispatchToStore(target)}
               fullWidth
             />
             <TextField
-              label='ZIP'
-              id='ZIP_to'
-              type='text'
-              variant='standard'
+              label="ZIP"
+              id="ZIP_to"
+              type="text"
+              variant="standard"
               value={toZIP}
-              onChange={({ target }) => dispatch(updateToZIP(target.value))}
-              name='toZIP'
+              onInput={({ target }) => dispatch(updateToZIP(target.value))}
+              {...register("recipientZip", { required: "Name required" })}
+              error={errors.recipientZip?.type ? true : false}
+              helperText={errors.recipientZip?.message}
               fullWidth
             />
             <TextField
-              label='City'
-              id='city_to'
-              type='text'
-              variant='standard'
+              label="City"
+              id="city_to"
+              type="text"
+              variant="standard"
               value={ship.recipientCity}
-              name='recipientCity'
-              onChange={({ target }) =>
-                dispatch(
-                  updateShipForm({ value: target.value, name: target.name })
-                )
-              }
+              {...register("recipientCity", { required: "Name required" })}
+              error={errors.recipientCity?.type ? true : false}
+              helperText={errors.recipientCity?.message}
+              onInput={({ target }) => dispatchToStore(target)}
               fullWidth
             />
             <TextField
               select
               fullWidth
-              label='State'
-              id='state_to'
+              label="State"
+              id="state_to"
               value={ship.recipientState}
-              name='recipientState'
-              onChange={({ target }) =>
-                dispatch(
-                  updateShipForm({ value: target.value, name: target.name })
-                )
-              }
-              variant='standard'
+              {...register("recipientState", { required: "Name required" })}
+              error={errors.recipientState?.type ? true : false}
+              helperText={errors.recipientState?.message}
+              onInput={({ target }) => dispatchToStore(target)}
+              variant="standard"
             >
               {usaStates.map(({ abbreviation, name }) => (
-                <MenuItem key={abbreviation + '_2'} value={abbreviation}>
+                <MenuItem key={abbreviation + "_2"} value={abbreviation}>
                   {name}
                 </MenuItem>
               ))}
             </TextField>
             <TextField
-              label='Phone'
-              id='phone_to'
-              type='tel'
-              variant='standard'
+              label="Phone (Optional)"
+              id="phone_to"
+              type="tel"
+              variant="standard"
               value={ship.recipientPhone}
-              name='recipientPhone'
-              onChange={({ target }) =>
-                dispatch(
-                  updateShipForm({ value: target.value, name: target.name })
-                )
-              }
+              name="recipientPhone"
+              onInput={({ target }) => dispatchToStore(target)}
               fullWidth
             />
             <TextField
-              label='Email'
-              id='email_to'
-              type='email'
-              variant='standard'
+              label="Email"
+              id="email_to"
+              type="email"
+              variant="standard"
               value={ship.recipientEmail}
-              name='recipientEmail'
-              onChange={({ target }) =>
-                dispatch(
-                  updateShipForm({ value: target.value, name: target.name })
-                )
-              }
+              {...register("recipientEmail", { required: "Name required" })}
+              error={errors.recipientEmail?.type ? true : false}
+              helperText={errors.recipientEmail?.message}
+              onInput={({ target }) => dispatchToStore(target)}
               fullWidth
             />
           </Stack>
@@ -293,16 +287,10 @@ const ShippingForm = () => {
         container
         sx={{ mt: 4 }}
         xs={12}
-        display={'flex'}
-        justifyContent={'center'}
+        display={"flex"}
+        justifyContent={"center"}
       >
-        <Button
-          sx={{ width: 1 / 2 }}
-          component={Link}
-          to='/ship-details'
-          variant='contained'
-          type='submit'
-        >
+        <Button sx={{ width: 1 / 2 }} variant="contained" type="submit">
           Continue
         </Button>
       </Grid>
