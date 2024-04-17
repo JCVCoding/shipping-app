463663a7009aaa9b91982cf4cdc6ffd141fb1537
josh <jvcoding9407@gmail.com>
2024-04-17 17:21:59 -0400

    Update account page to use the accounts shipment data from db

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -8,8 +8,30 @@ import {
   TableCell,
   Typography,
 } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useAppSelector } from "../hooks";
+
+type Shipment = {
+  accountNumber: string;
+  trackNumber: string;
+  deliveryDate: string;
+  shipDate: string;
+};
 
 const AccountPage = () => {
+  const accountNumber = useAppSelector(
+    (state) => state.user.accounts[0].accountNumber
+  );
+  const [shipments, setShipments] = useState<Shipment[]>([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/ship/${accountNumber}`, {
+      method: "get",
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((data) => data.json())
+      .then((shipments) => setShipments(shipments));
+  }, [accountNumber]);
   return (
     <>
       <Typography>Account Information</Typography>
@@ -25,15 +47,17 @@ const AccountPage = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            <TableRow>
-              <TableCell>12345</TableCell>
-              <TableCell>4/17/2024</TableCell>
-              <TableCell>2ZXXXX</TableCell>
-              <TableCell>4/21/2024</TableCell>
-              <TableCell>
-                <Chip label="In Transit" color="info" />
-              </TableCell>
-            </TableRow>
+            {shipments.map((shipment) => (
+              <TableRow key={shipment.trackNumber}>
+                <TableCell>{shipment.accountNumber}</TableCell>
+                <TableCell>{shipment.shipDate}</TableCell>
+                <TableCell>{shipment.trackNumber}</TableCell>
+                <TableCell>{shipment.deliveryDate}</TableCell>
+                <TableCell>
+                  <Chip label="In Transit" color="info" />
+                </TableCell>
+              </TableRow>
+            ))}
           </TableBody>
         </Table>
       </TableContainer>
